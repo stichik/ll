bacc432f8f58adabdb3c21e84945d6d5bd344e30
stichik <93238627+stichik@users.noreply.github.com>
2021-10-27 09:00:18 +0500

    Add files via upload

---
+++ ConsoleApplication12.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[10];
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "a[" << i << "] "; cin >> a[i];
+    }
+    cout << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "a[" << i << "] = " << a[i] << ",";
+    }
+}
